7393c06e63df34521b52fa8b08b5fb7fd76bec08
devopsUser93 <devopsuser93@gmail.com>
2022-11-12 21:15:01 +0530

    devops second try

--- sample.cs
+++ sample.cs
@@ -7,6 +7,7 @@ void main(char[] args)
     Console.WriteLine("press any key to exit");
     // added comment by user devops user
     // added comments again by bits user
+    // devops second try
     Console.ReadLine();
     Console.WriteLine("Thank you");
 }
